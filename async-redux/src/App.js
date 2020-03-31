cd0f1c4c032c49c3c5329945f414202afe29fe20
josepaniagua <jpanjr@gmail.com>
2020-03-31 00:21:38 -0700

    added mapstate and mapdispatch to App.js

--- async-redux/src/App.js
+++ async-redux/src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStars } from "./actions";
+import Character from "./components/Character";
+
 import "./App.css";
 
-const App = () => {
+const App = props => {
+  useEffect(() => {
+    props.fetchStars();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Star</h1>
-      <h1>Wars</h1>
-      <h1>Characters</h1>
+      <h1>StarWars Characters</h1>
+      {props.starError && <p>Error => {props.starError}</p>}
+
+      {props.isStarLoading ? <h2>Still Loading...</h2> : <Character />}
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isStarLoading: state.isLoading,
+    starName: state.name,
+    starError: state.error
+  };
+};
+
+const mapDispatchToState = {
+  fetchStars
+};
+
+export default connect(mapStateToProps, mapDispatchToState)(App);
