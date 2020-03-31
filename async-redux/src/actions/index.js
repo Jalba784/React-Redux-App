73f83759d9ef6d55bb852f47473d6bf548097ee6
josepaniagua <jpanjr@gmail.com>
2020-03-30 20:02:07 -0700

    latest update

--- async-redux/src/actions/index.js
+++ async-redux/src/actions/index.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const FETCH_STAR_START = 'FETCH_STAR_START';
 export const FETCH_STAR_SUCCESS = 'FETCH_STAR_SUCCESS';
 export const FETCH_STAR_ERROR = 'FETCH_STAR_ERROR';
+
+export const fetchStars = () => {
+    sdf
+};
