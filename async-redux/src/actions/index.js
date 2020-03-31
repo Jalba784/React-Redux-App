de6a26aa8fef2847acc5a747ec9b4b036937cdea
josepaniagua <jpanjr@gmail.com>
2020-03-30 20:11:40 -0700

    finished with index in actions directory

--- async-redux/src/actions/index.js
+++ async-redux/src/actions/index.js
@@ -5,5 +5,13 @@ export const FETCH_STAR_SUCCESS = 'FETCH_STAR_SUCCESS';
 export const FETCH_STAR_ERROR = 'FETCH_STAR_ERROR';
 
 export const fetchStars = () => {
-    sdf
+    return dispatch => {
+        axios.get('https://swapi.co/api/people')
+            .then(res => {
+                dispatch({ type: FETCH_STAR_SUCCESS, payload: res.data.results })
+            })
+            .catch(err => {
+                dispatch({ type: FETCH_STAR_ERROR, payload: err })
+            })
+    }
 };
