83f831ea7baa5c5edb961e684e2dba16cc46a357
josepaniagua <jpanjr@gmail.com>
2020-03-30 20:00:15 -0700

    initial commit and actions directory

---
+++ async-redux/src/actions/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const FETCH_STAR_START = 'FETCH_STAR_START';
+export const FETCH_STAR_SUCCESS = 'FETCH_STAR_SUCCESS';
+export const FETCH_STAR_ERROR = 'FETCH_STAR_ERROR';
